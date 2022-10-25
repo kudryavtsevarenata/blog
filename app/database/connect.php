630b9b437e94a4659f3da938ac6a49cdaf7a3eba
kudryavtsevarenata <kudryavtseva.renata@yandex.ru>
2022-10-25 20:15:57 +0500

    insert, update and delete parsers for db

--- app/database/connect.php
+++ app/database/connect.php
@@ -5,7 +5,8 @@ $db_name = 'dinamic_site';
 $db_user ='root';
 $db_pass = '';
 $charset = 'utf8';
-$options = [PDO:: ATTR_ERRMODE => PDO:: ERRMODE_EXCEPTION];
+$options = [PDO:: ATTR_ERRMODE => PDO:: ERRMODE_EXCEPTION,
+            PDO:: ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
 
 try{
     $pdo = new PDO(
--- app/database/db.php
+++ app/database/db.php
@@ -7,22 +7,151 @@ function tt($value)
     print_r($value);
     echo '</pre>';
 }
-
-function selectAll($table)
+// проверка запроса
+function dbCheckError($query)
 {
-    global $pdo;
-    $sql = "SELECT * FROM dinamic_site.$table";
-    $query = $pdo->prepare($sql);
-    $query->execute();
     $errInfo = $query->errorInfo();
     if ($errInfo[0] !== PDO::ERR_NONE)
     {
         echo $errInfo[2];
         exit();
     }
+    return true;
+}
+
+function selectAll($table, $params = [])
+{
+    global $pdo;
+    $sql = "SELECT * FROM dinamic_site.$table";
+    if (!empty($params))
+    {
+        $i = 0;
+        foreach ($params as $key => $value)
+        {
+            if (!is_numeric($value))
+            {
+                $value = "'" . $value . "'";
+            }
+            if ($i === 0)
+            {
+                $sql = $sql . " WHERE $key = $value ";
+            }
+            else{
+                $sql = $sql . "AND $key = $value ";
+            }
+            $i++;
+        }
+    }
+    // tt($sql);
+    // exit();
+    $query = $pdo->prepare($sql);
+    $query->execute();
+    dbCheckError($query);
     $date = $query->fetchAll();
     return $date;
 }
 
-tt(selectAll('users'));
+function selectOne($table, $params = [])
+{
+    global $pdo;
+    $sql = "SELECT * FROM dinamic_site.$table";
+    if (!empty($params))
+    {
+        $i = 0;
+        foreach ($params as $key => $value)
+        {
+            if (!is_numeric($value))
+            {
+                $value = "'" . $value . "'";
+            }
+            if ($i === 0)
+            {
+                $sql = $sql . " WHERE $key = $value ";
+            }
+            else{
+                $sql = $sql . "AND $key = $value ";
+            }
+            $i++;
+        }
+    }
+    // $sql = $sql . "LIMIT 1";
+    $query = $pdo->prepare($sql);
+    $query->execute();
+    dbCheckError($query);
+    $date = $query->fetch();
+    return $date;
+}
+
+function insert($table, $params)
+{
+    global $pdo;
+    $i = 0;
+    $col = "";
+    $mask = "";
+    foreach ($params as $key => $value)
+    {
+        if ($i === 0)
+        {
+            $col = $col . $key;
+            $mask = $mask . "'" . $value . "'";
+        }
+        else{
+            $col = $col . ", $key";
+            $mask = $mask . ", '$value'";
+        }
+        $i++;
+    }
+    $sql = "INSERT INTO dinamic_site.$table ($col) VALUES ($mask)";
+    $query = $pdo->prepare($sql);
+    $query->execute($params);
+    dbCheckError($query);
+}
+
+function update($table, $id, $params)
+{
+    global $pdo;
+    $i = 0;
+    $str = "";
+    foreach ($params as $key => $value)
+    {
+        if ($i === 0)
+        {
+            $str = $str . $key . " = '" . $value . "'";
+        }
+        else{
+            $str = $str . ", " . $key . " = '" . $value . "'";
+        }
+        $i++;
+    }
+    $sql = "UPDATE dinamic_site.$table SET $str WHERE id = $id;";
+    $query = $pdo->prepare($sql);
+    $query->execute($params);
+    dbCheckError($query);
+}
+
+function delete($table, $id)
+{
+    global $pdo;
+    $sql = "DELETE FROM dinamic_site.$table WHERE id = $id;";
+    $query = $pdo->prepare($sql);
+    $query->execute();
+    dbCheckError($query);
+}
+
+$arrData =[
+    'admin' => '0',
+    'username' => '228',
+    'email' => 're228@re.ru',
+    'password' => '121212'
+];
+
+$params = [
+    'admin' => 0,
+    'username' => 'Powered'
+];
+// tt(selectOne('users', $params));
+// tt(selectAll('users', $params));
+// insert('users', $arrData);
+// update('users', 5, $params)
+delete('users', 5);
 ?>
